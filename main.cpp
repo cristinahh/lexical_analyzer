6cb9c1726231db09baa285f81109214b2232320a
michaelbabinec <55720544+michaelbabinec@users.noreply.github.com>
2023-10-03 17:53:46 -0700

    Updates towards finalization

--- main.cpp
+++ main.cpp
@@ -1,9 +1,9 @@
-#include <string>
 #include <iostream>
 #include <utility>
 #include <vector>
 #include <cctype>
 #include <fstream>
+#include <string>
 
 using namespace std;
 
@@ -13,10 +13,13 @@ vector<pair<string, string > > myVect;
 //This will store the characters of the string currently processed by lexer
 vector<char> lexeme;
 
+int countNeg = 0; // keeps track of negative signs. Used inside isReal. 
+int countDec = 0; // keeps track of decimal signs. Used inside isReal.
+
 //The word is passed here for checking if keyword
 bool isKeyword(string word)
 {
-    if( word == "int" || word == "while" || word == "else" || word == "if" || word == "double"|| word == "string")
+    if( word == "int" || word == "while" || word == "else" || word == "if" || word == "double"|| word == "string"|| word == "for")
     {
         return true;
     }
@@ -49,51 +52,53 @@ bool isIdentifier(string word)
 bool isOperator(string word)
 {
     if(word == "+" || word == "-" || word == "/" || word == "%" || 
-    word == "*" || word == "<" || word == ">" || word == ">=" || word == "<=" || word == "==" || word == "=")
+    word == "*" || word == "<" || word == ">" || word == ">=" || 
+    word == "<=" || word == "==" || word == "=")
     {
         return true;
     }
     return false;
 
 }
-//The word is passed here for checking if keyword
-//a loop is ran to determine if the value is negative or unreal
+
+//a loop is ran to determine if the value is negative or unreal. 
+//Also checks if number entry is valid by counting '-' and '.'. More than 1 is invalid
 bool isReal(string word)
 {
+    bool isR = true;
     
     for(int i = 0; i < word.length(); i++)
     {
-        int countNeg = 0;
-        int countDec = 0;
         
-        if( word[0] == '-' && isdigit(word[1]))
+        if(isdigit(word[i]) && countNeg < 2  && countDec < 2)
         {
-            if(countNeg == 0)
+
+            continue;
+        }
+        else if( word[i] == '-' && isdigit(word[i+1]))
+        {
+            countNeg++;
+            if(countNeg >1)
             {
-                countNeg++;
-                break;
-            }
-            else
                 return false;
-        }
-        else if(isdigit(word[i]))
-        {
-            break;
-        }
-        else if ( word[i] == '.' )
-        {
-            if(countDec == 0)
-            {
-            countDec++;
-            break;
             }
-            else 
-            return false;
+        }
+        else if (word[i] == '.' && isdigit(word[i+1]))
+        {
+            countDec++;
+           
+            if(countDec >1)
+            {
+                return false;
+            }
+           
         }
         else
-            return false;
+        {
+            isR = false;
+        }
     }
-    return true;
+    return isR;
 }
 
 
@@ -106,12 +111,12 @@ void createPair( string token, string lexeme)
 
 //The vector to be lexed is passed here & identified
 //This is the primary lexing function
-
-    void checkToken(vector<char> lexVect)
+void checkToken(vector<char> lexVect)
 {
     // This concatenates the vector into a string
     string lexStr(lexVect.begin(), lexVect.end());
 
+    // each string is checked to identify token
     if (isKeyword(lexStr))
     {
         createPair("keyword", lexStr);
@@ -128,12 +133,15 @@ void createPair( string token, string lexeme)
     {
         createPair("real", lexStr);
     }
-    else
+    else if( isIdentifier(lexStr))
     {
         createPair("identifier", lexStr);
     }
+    else 
+    {
+        cout<< "Not a valid statement"<< endl;
+    }
 }
-
 
 
 // This clears the lexing vector after determination of the specific key
@@ -148,6 +156,8 @@ vector<char> clearVect(vector<char>& vect)
 }
 
 
+/*
+
 // This is the print function for the Output myVect pairs
 void print(vector<pair<string,string > > vect)
 {
@@ -157,13 +167,28 @@ void print(vector<pair<string,string > > vect)
     }
 }
 
+*/
+
+void print(ostream & fout, vector<pair<string,string > > vect)
+{
+    for ( int i = 0; i < vect.size(); i++)
+    {
+        fout<< ( vect[i].first) << ", " << vect[i].second << endl;
+    }
+}
+
+
 //This function makes the input string something we can actually parse
 string stringFix(string input)
 {
     vector<char> outputVect;
     
     for (int i = 0; i < input.length(); ++i)
-    {
+    {   
+        if(input[i] == ' ' && i == 0) //gets rid of space if it is in the front of the string
+        {
+            continue;
+        }
         //Alphanumeric & Spaces are Okay to push
         if (isalpha(input[i]))
         {
@@ -190,9 +215,9 @@ string stringFix(string input)
 	 // This allows !=, ==, <<, etc statements to be pushed without a space between them
         else if( input[i] == '=' || input[i] == '+' || input[i] == '/' || input[i] == '%' 
         || input[i] == '&'|| input[i] == '|'|| input[i] == '<' || input[i] == '>' || input[i] == '!' || input[i] == '.' )
-	{ 
-		outputVect.push_back(input[i]);  
-	}
+	    { 
+		    outputVect.push_back(input[i]);  
+	    }
 		
         else if (input[i] == '(' || ')' || ';')
         {
@@ -205,10 +230,10 @@ string stringFix(string input)
             //Push the operator
             outputVect.push_back(input[i]);
             
-            //If there's no space after, add one
-            if(input[(i+1)] != ' ')
+            //If there's no space after, add one, unless it is the last parenthesis.
+            if(input[(i+1)] != ' ' && input[i+1] != ';')
             {
-                outputVect.push_back(' ');
+                    outputVect.push_back(' ');
             }
         }
         
@@ -216,46 +241,56 @@ string stringFix(string input)
     }
     
     //Restring the Vector
-   string outputstring(outputVect.begin(), outputVect.end());
+    string outputstring(outputVect.begin(), outputVect.end());
     return outputstring;
 }
 
 // Driver Code
 int main()
 {
-    string inputFileName = "input.txt"; 
+    //input_scode.txt is our input file
+    string inputFileName = "input_scode.txt"; 
     ifstream inputFile(inputFileName);
+   
     if (!inputFile.is_open())
     {
         cout << "Error! Failed to open input file. " << endl;
         return 0;
     }
+
     string input;
     getline(inputFile, input); 
     inputFile.close();
 
-    cout << "Input from file: " << input << endl;
+    // cout << "Input from file: " << input << endl;               // This is string you start with
     string checkStr = stringFix(input);
+   // cout << "String fixed with spaces : " << checkStr << endl;  // This is string with appropriate spaces
     //string checkStr = "while ( t < upper ) s = 22.00 ;";
 
     for ( int i = 0; i < checkStr.length(); i++ ) {
         if(checkStr[0] == ' ') {
-        break;
+            continue;
         }
-         if(checkStr[i] != ' ') {
+         else if(checkStr[i] != ' ') {
             lexeme.push_back(checkStr[i]);
             if (i == checkStr.length()-1)
             {
                 checkToken(lexeme); 
             }
         }
-        else if (checkStr[i] == ' ' ) {
+        else if (checkStr[i] == ' ' && i != 0 ) {
             //Identify the Lexeme
             checkToken(lexeme);
             //Clear the Lexeme vect for the next iteration
             clearVect(lexeme);
         }
     }
-   print(myVect);
+
+ofstream fileOutput;
+fileOutput.open("output.txt");
+
+print(fileOutput, myVect);
+
+fileOutput.close();
 	return 0;
 }
